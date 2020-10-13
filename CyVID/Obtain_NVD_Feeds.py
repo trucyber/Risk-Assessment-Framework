14cb368ba3005d31d0ebe352d22d281d9f6c7084
Adeel M <callmead@gmail.com>
2020-10-13 16:13:24 -0600

    Update Obtain_NVD_Feeds.py

--- CyVID/Obtain_NVD_Feeds.py
+++ CyVID/Obtain_NVD_Feeds.py
@@ -9,55 +9,59 @@ from os.path import isfile, join
 json_files_path = 'nvd_json_files' # JSON files path globally
 json_feed_url = 'https://nvd.nist.gov/vuln/data-feeds#JSON_FEED'
 
-# 1. Download files...
-def download_json_files(feed_url):
+# 1. Check existing files and folders
+def check_files(dir_path):
     # Check if the directory for json files exists, if not create one.
-    if (os.path.isdir(os.getcwd()+'\\'+json_files_path)): # if folder does not exist, create one
-        print('Directory',json_files_path,'already exists.')
+    if (os.path.isdir(os.getcwd()+'\\'+dir_path)): # if folder does not exist, create one
+        print('Directory',dir_path,'already exists.')
         # remove old files from the directory
         print('Removing old files...')
-        files = glob.glob(os.getcwd()+'\\'+json_files_path+'\\*')
+        files = glob.glob(os.getcwd()+'\\'+dir_path+'\\*')
         for f in files:
-            os.remove(f)    
+            os.remove(f)
+        print('Done.\n')
     else:
-        print('Creating directory',json_files_path, 'to store JSON feeds.')
+        print('Creating directory',dir_path, 'to store feeds.')
         current_path = os.getcwd()
-        directory = os.path.join(current_path+'\\', json_files_path) 
+        directory = os.path.join(current_path+'\\', dir_path) 
         os.mkdir(directory)
-
+        print('Done.\n')
+        
+# 2. Download files...
+def download_json_files(feed_url):
     # Start obtaining new feeds...
-    print('\nObtaining JSON feeds from NVD...\n['+json_feed_url+']')
+    print('Obtaining NVD feeds from: '+json_feed_url)
     r = requests.get(feed_url) # where the files are...
     for filename in re.findall('nvdcve-1.1-[0-9]*\.json\.zip', r.text): # yearly file names e.g. nvdcve-1.1-2020.json.zip 
-        print('File ' + filename +' found. ', end='')
+        print('File ' + filename +' found.', end=' ')
         r_file = requests.get('https://nvd.nist.gov/feeds/json/cve/1.1/' + filename, stream=True)
         with open(json_files_path + '/' + filename, 'wb') as f:
-            print('Downloading... ', end='')
+            print('Downloading...', end=' ')
             for chunk in r_file.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
-            print('done.')
+            print('Done.')
 
 # 2. Unzip and Delete zip files...
 # Extract the JSON files from the downloaded zip files from the previous step, and remove the zip files.
-def extract_and_remove_json():
-    print('\nExtracting JSON files.')
-    files = [f for f in listdir(json_files_path + '/' ) if isfile(join(json_files_path, f))]
+def extract_and_remove_zips(file_path):
+    print('\nPreparing files.')
+    files = [f for f in listdir(file_path + '/' ) if isfile(join(file_path, f))]
     files.sort()
     for file in files:
-        archive = zipfile.ZipFile(join(json_files_path + '/' , file), 'r')
-        print('Extracting ' + join(json_files_path + '/' , file) + '... ', end='')
-        archive.extractall(os.getcwd()+'\\'+json_files_path) # add here
+        archive = zipfile.ZipFile(join(file_path + '/' , file), 'r')
+        print('Extracting ' + join(file_path + '/' , file) + '... ', end='')
+        archive.extractall(os.getcwd()+'\\'+file_path) # add here
         archive.close()
-        print('done. ', end='')
+        print('Done. ', end='')
         # remove zip files after extraction
         if file.endswith('.zip'):
             print('Removing zip file' + '... ', end='')
             try:
-                os.remove(join(json_files_path + '/' , file))
-                print('done.')
+                os.remove(join(file_path + '/' , file))
+                print('Done.')
             except PermissionError:
-                print('Error removing file:', join(json_files_path + '/' , file))    
+                print('Error removing file:', join(file_path + '/' , file))   
     
 print('*** NVD JSON Handler v3 ***\n')
 
@@ -65,10 +69,13 @@ print('*** NVD JSON Handler v3 ***\n')
 start = timeit.default_timer() # start timer
 
 # Step 1
-download_json_files(json_feed_url)
+check_files(json_files_path)
 
 # Step 2
-extract_and_remove_json()
+download_json_files(json_feed_url)
+
+# Step 3
+extract_and_remove_zips(json_files_path)
 
 stop = timeit.default_timer()
 execution_time = stop - start
